5deb2cfee4065318bf85df64150eebd39eb07238
haozz <haozz0611@163.com>
2019-12-31 14:24:48 +0800

    HibernateValidator

---
+++ src/main/java/com/haozz/dailylearn/dailylearn201912/dailylearn_20191231/HibernateValidator.java
@@ -0,0 +1,155 @@
+package com.haozz.dailylearn.dailylearn201912.dailylearn_20191231;
+
+import lombok.Data;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 基于HibernateValidator的参数验证器
+ *
+ * @author haozhezhe@yunquna.com
+ * @date 14:08 2019/12/31
+ */
+public class HibernateValidator {
+    /**
+     * 开启快速失败模式 failFast (true)
+     */
+    private static Validator validator = Validation.byProvider(org.hibernate.validator.HibernateValidator.class)
+            .configure().failFast(false).buildValidatorFactory().getValidator();
+
+    /**
+     * 校验对象
+     *
+     * @param t      bean
+     * @param groups 校验组
+     * @return ValidResult
+     */
+    public static <T> void validate(T t, Class<?>... groups) {
+        Set<ConstraintViolation<T>> violationSet = validator.validate(t, groups);
+        boolean hasError = violationSet != null && violationSet.size() > 0;
+        if (hasError) {
+            StringBuilder sb = new StringBuilder();
+            for (ConstraintViolation<T> violation : violationSet) {
+                sb.append(violation.getPropertyPath().toString()).append(violation.getMessage()).append(";");
+            }
+            throw new RuntimeException(sb.toString());
+        }
+    }
+
+    /**
+     * 校验对象
+     *
+     * @param t      bean
+     * @param groups 校验组
+     * @return ValidResult
+     */
+    public static <T> HibernateValidator.ValidResult validateBean(T t, Class<?>... groups) {
+        HibernateValidator.ValidResult result = new HibernateValidator().new ValidResult();
+        Set<ConstraintViolation<T>> violationSet = validator.validate(t, groups);
+        boolean hasError = violationSet != null && violationSet.size() > 0;
+        result.setHasErrors(hasError);
+        if (hasError) {
+            for (ConstraintViolation<T> violation : violationSet) {
+                result.addError(violation.getPropertyPath().toString(), violation.getMessage());
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 校验bean的某一个属性
+     *
+     * @param obj          bean
+     * @param propertyName 属性名称
+     * @return ValidResult
+     */
+    public static <T> HibernateValidator.ValidResult validateProperty(T obj, String propertyName) {
+        HibernateValidator.ValidResult result = new HibernateValidator().new ValidResult();
+        Set<ConstraintViolation<T>> violationSet = validator.validateProperty(obj, propertyName);
+        boolean hasError = violationSet != null && violationSet.size() > 0;
+        result.setHasErrors(hasError);
+        if (hasError) {
+            for (ConstraintViolation<T> violation : violationSet) {
+                result.addError(propertyName, violation.getMessage());
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 校验结果类
+     */
+    @Data
+    public class ValidResult {
+
+        /**
+         * 是否有错误
+         */
+        private boolean hasErrors;
+
+        /**
+         * 错误信息
+         */
+        private List<HibernateValidator.ErrorMessage> errors;
+
+        public ValidResult() {
+            this.errors = new ArrayList<ErrorMessage>();
+        }
+
+        public boolean hasErrors() {
+            return hasErrors;
+        }
+
+        public void setHasErrors(boolean hasErrors) {
+            this.hasErrors = hasErrors;
+        }
+
+        /**
+         * 获取所有验证信息
+         *
+         * @return 集合形式
+         */
+        public List<ErrorMessage> getAllErrors() {
+            return errors;
+        }
+
+        /**
+         * 获取所有验证信息
+         *
+         * @return 字符串形式
+         */
+        public String getErrors() {
+            StringBuilder sb = new StringBuilder();
+            for (HibernateValidator.ErrorMessage error : errors) {
+                sb.append(error.getPropertyPath()).append(":").append(error.getMessage()).append(" ");
+            }
+            return sb.toString();
+        }
+
+        public void addError(String propertyName, String message) {
+            this.errors.add(new HibernateValidator.ErrorMessage(propertyName, message));
+        }
+    }
+
+    @Data
+    public class ErrorMessage {
+
+        private String propertyPath;
+
+        private String message;
+
+        public ErrorMessage() {
+        }
+
+        public ErrorMessage(String propertyPath, String message) {
+            this.propertyPath = propertyPath;
+            this.message = message;
+        }
+    }
+
+}
