1949fe593971386d2dbecadad84426a084fe7ea8
Hertz <40011192+haozz1994@users.noreply.github.com>
2020-01-04 16:26:53 +0800

    test mybatis-plus

--- src/test/java/com/haozz/dailylearn/SimpleTest.java
+++ src/test/java/com/haozz/dailylearn/SimpleTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author
@@ -24,10 +26,19 @@ public class SimpleTest {
 
     @Test
     public void testMyBatisPlus() {
-
         List<User> list = userMapper.selectList(null);
         System.out.println(list);
         Assert.isTrue(list.size() == 1, "查询失败");
+    }
+
+    @Test
+    public void testByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "ROBIN");
+        map.put("age", "30");
+        //where name= 'ROBIN' and age= 30;
+        List<User> list = userMapper.selectByMap(map);
+        System.out.println("==============" + list);
 
     }
 }
