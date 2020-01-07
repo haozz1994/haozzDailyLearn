ac19db22f37a074ab5beaf0e175b8465782e3f79
haozz <haozz0611@163.com>
2020-01-07 16:47:57 +0800

    test

---
+++ src/main/java/com/haozz/dailylearn/dailylearn202001/dailylearn_20200107/DailyLearn20200107.java
@@ -0,0 +1,14 @@
+package com.haozz.dailylearn.dailylearn202001.dailylearn_20200107;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 16:46 2020/1/7
+ */
+public class DailyLearn20200107 {
+
+    public static void main(String[] args) {
+        String a = null;
+        a += "aaa";
+        System.out.println(a);
+    }
+}
