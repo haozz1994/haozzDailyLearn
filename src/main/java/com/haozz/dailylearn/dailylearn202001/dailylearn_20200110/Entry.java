8faef6cc74401d5f193e9852ef4463af5d2f1443
haozz <haozz0611@163.com>
2020-01-10 22:14:03 +0800

    leetCode

---
+++ src/main/java/com/haozz/dailylearn/dailylearn202001/dailylearn_20200110/Entry.java
@@ -0,0 +1,31 @@
+package com.haozz.dailylearn.dailylearn202001.dailylearn_20200110;
+
+/**
+ * @author: haozz
+ * @date: 2020/1/10 21:57
+ */
+public class Entry {
+
+    private int key;
+
+    private int value;
+
+    public Entry() {
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
