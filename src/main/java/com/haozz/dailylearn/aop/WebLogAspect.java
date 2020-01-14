e35bb61bc684495c41bcdd25f9bcdb3686b3b08c
haozz <haozz0611@163.com>
2020-01-14 23:43:38 +0800

    spring aop demo

--- src/main/java/com/haozz/dailylearn/aop/WebLogAspect.java
+++ src/main/java/com/haozz/dailylearn/aop/WebLogAspect.java
@@ -3,10 +3,7 @@ package com.haozz.dailylearn.aop;
 import com.alibaba.druid.support.json.JSONUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -62,19 +59,15 @@ public class WebLogAspect {
     }
 
 
-
-
-
-
-
-
     //===========================================================================================
+
     /**
      * 前置通知，方法调用前被调用
+     *
      * @param joinPoint/null
      */
     @Before("webLog()")
-    public void before(JoinPoint joinPoint){
+    public void before(JoinPoint joinPoint) {
         logger.info("前置通知");
         //获取目标方法的参数信息
         Object[] obj = joinPoint.getArgs();
@@ -85,9 +78,9 @@ public class WebLogAspect {
         //用的最多 通知的签名
         Signature signature = joinPoint.getSignature();
         //代理的是哪一个方法
-        logger.info("代理的是哪一个方法"+signature.getName());
+        logger.info("代理的是哪一个方法" + signature.getName());
         //AOP代理类的名字
-        logger.info("AOP代理类的名字"+signature.getDeclaringTypeName());
+        logger.info("AOP代理类的名字" + signature.getDeclaringTypeName());
         //AOP代理类的类（class）信息
         signature.getDeclaringType();
         //获取RequestAttributes
@@ -98,14 +91,14 @@ public class WebLogAspect {
         HttpSession session = (HttpSession) requestAttributes.resolveReference(RequestAttributes.REFERENCE_SESSION);
         //获取请求参数
         Enumeration<String> enumeration = request.getParameterNames();
-        Map<String,String> parameterMap = new HashMap();
-        while (enumeration.hasMoreElements()){
+        Map<String, String> parameterMap = new HashMap();
+        while (enumeration.hasMoreElements()) {
             String parameter = enumeration.nextElement();
-            parameterMap.put(parameter,request.getParameter(parameter));
+            parameterMap.put(parameter, request.getParameter(parameter));
         }
         String str = JSONUtils.toJSONString(parameterMap);
-        if(obj.length > 0) {
-            logger.info("请求的参数信息为："+str);
+        if (obj.length > 0) {
+            logger.info("请求的参数信息为：" + str);
         }
     }
 
@@ -113,22 +106,41 @@ public class WebLogAspect {
     /**
      * 后置返回通知
      * 这里需要注意的是:
-     *      如果参数中的第一个参数为JoinPoint，则第二个参数为返回值的信息
-     *      如果参数中的第一个参数不为JoinPoint，则第一个参数为returning中对应的参数
-     *       returning：限定了只有目标方法返回值与通知方法相应参数类型时才能执行后置返回通知，否则不执行，
-     *       对于returning对应的通知方法参数为Object类型将匹配任何目标返回值
+     * 如果参数中的第一个参数为JoinPoint，则第二个参数为返回值的信息
+     * 如果参数中的第一个参数不为JoinPoint，则第一个参数为returning中对应的参数
+     * returning：限定了只有目标方法返回值与通知方法相应参数类型时才能执行后置返回通知，否则不执行，
+     * 对于returning对应的通知方法参数为Object类型将匹配任何目标返回值
+     *
      * @param joinPoint
      * @param keys
      */
-    @AfterReturning(pointcut = "webLog()",returning = "keys")
-    public void doAfterReturningAdvice1(JoinPoint joinPoint,Object keys){
-        logger.info("第一个后置返回通知的返回值："+keys);
+    @AfterReturning(pointcut = "webLog()", returning = "keys")
+    public void doAfterReturningAdvice1(JoinPoint joinPoint, Object keys) {
+        logger.info("第一个后置返回通知的返回值：" + keys);
     }
 
-    @AfterReturning(pointcut = "webLog()",returning = "keys",argNames = "keys")
-    public void doAfterReturningAdvice2(String keys){
-        logger.info("第二个后置返回通知的返回值："+keys);
+    @AfterReturning(pointcut = "webLog()", returning = "keys", argNames = "keys")
+    public void doAfterReturningAdvice2(String keys) {
+        logger.info("第二个后置返回通知的返回值：" + keys);
     }
 
+
+    /**
+     * 后置异常通知
+     * 定义一个名字，该名字用于匹配通知实现方法的一个参数名，当目标方法抛出异常返回后，将把目标方法抛出的异常传给通知方法；
+     * throwing:限定了只有目标方法抛出的异常与通知方法相应参数异常类型时才能执行后置异常通知，否则不执行，
+     * 对于throwing对应的通知方法参数为Throwable类型将匹配任何异常。
+     *
+     * @param joinPoint
+     * @param exception
+     */
+    @AfterThrowing(pointcut = "webLog()", throwing = "exception")
+    public void doAfterThrowingAdvice(JoinPoint joinPoint, Throwable exception) {
+        //目标方法名：
+        logger.info(joinPoint.getSignature().getName());
+        if (exception instanceof NullPointerException) {
+            logger.info("发生了空指针异常!!!!!");
+        }
+    }
 
 }
