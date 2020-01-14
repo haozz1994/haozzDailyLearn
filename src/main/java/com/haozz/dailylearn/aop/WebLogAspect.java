341c97f866ae9b3f65a0bec9e99d63a19cc9a5b2
haozz <haozz0611@163.com>
2020-01-14 23:40:36 +0800

    spring aop demo

--- src/main/java/com/haozz/dailylearn/aop/WebLogAspect.java
+++ src/main/java/com/haozz/dailylearn/aop/WebLogAspect.java
@@ -108,4 +108,27 @@ public class WebLogAspect {
             logger.info("请求的参数信息为："+str);
         }
     }
+
+
+    /**
+     * 后置返回通知
+     * 这里需要注意的是:
+     *      如果参数中的第一个参数为JoinPoint，则第二个参数为返回值的信息
+     *      如果参数中的第一个参数不为JoinPoint，则第一个参数为returning中对应的参数
+     *       returning：限定了只有目标方法返回值与通知方法相应参数类型时才能执行后置返回通知，否则不执行，
+     *       对于returning对应的通知方法参数为Object类型将匹配任何目标返回值
+     * @param joinPoint
+     * @param keys
+     */
+    @AfterReturning(pointcut = "webLog()",returning = "keys")
+    public void doAfterReturningAdvice1(JoinPoint joinPoint,Object keys){
+        logger.info("第一个后置返回通知的返回值："+keys);
+    }
+
+    @AfterReturning(pointcut = "webLog()",returning = "keys",argNames = "keys")
+    public void doAfterReturningAdvice2(String keys){
+        logger.info("第二个后置返回通知的返回值："+keys);
+    }
+
+
 }
