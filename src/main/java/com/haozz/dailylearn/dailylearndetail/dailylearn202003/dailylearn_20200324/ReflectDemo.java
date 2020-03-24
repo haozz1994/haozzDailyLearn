6899b9ba370fbf54af97c2c168db56e8b807820c
haozz1994 <haozz0611@163.com>
2020-03-24 23:41:52 +0800

    executor config

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202003/dailylearn_20200324/ReflectDemo.java
@@ -0,0 +1,38 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202003.dailylearn_20200324;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * 通过反射获取类中所有方法
+ *
+ * @author: haozz
+ * @date: 2020/3/24 23:28
+ */
+public class ReflectDemo {
+
+    public void methodA(){
+
+    }
+
+    private void methodB(){
+
+    }
+
+    public static void main(String[] args) {
+
+        Class<ReflectDemo> reflectDemoClass = ReflectDemo.class;
+
+        //方案一：获取当前类及其父类所有的公有方法
+        Method[] methods = reflectDemoClass.getMethods();
+        Arrays.asList(methods).forEach(method -> System.out.println(method.getName()));
+
+        System.out.println("==============================");
+
+        //方案二：获取当前类中所有的方法
+        Method[] declaredMethods = reflectDemoClass.getDeclaredMethods();
+        Arrays.asList(declaredMethods).forEach(declaredMethod -> System.out.println(declaredMethod.getName()));
+
+    }
+
+}
