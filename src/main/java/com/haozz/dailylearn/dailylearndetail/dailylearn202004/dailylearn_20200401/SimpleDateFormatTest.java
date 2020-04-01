8f8d0893d20906783b0d10957bc905ee5f186653
haozz <haozz0611@163.com>
2020-04-01 14:31:07 +0800

    20200401

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202004/dailylearn_20200401/SimpleDateFormatTest.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202004/dailylearn_20200401/SimpleDateFormatTest.java
@@ -10,6 +10,12 @@ import java.util.concurrent.Executors;
  * SimpleDateFormat线程不安全测试
  * https://blog.csdn.net/zxh87/article/details/19414885
  *
+ * 解决方案：
+ *      * 1.每个线程内部创建SimpleDateFormat实例。 ×不推荐
+ *      * 2.同步SimpleDateFormat对象。  ×不推荐，会降低并发性
+ *      * 3.使用其他类库工具。   √推荐
+ *      * 4.使用ThreadLocal。   √推荐
+ *
  * @author haozhezhe@yunquna.com
  * @date 10:33 2020/4/1
  */
@@ -45,10 +51,7 @@ public class SimpleDateFormatTest {
     }
 
     /**
-     * 解决方案：
-     * 1.每个线程内部创建SimpleDateFormat实例。 ×不推荐
-     * 2.同步SimpleDateFormat对象。  ×不推荐，会降低并发性
-     * 3.使用其他类库工具。   √推荐
-     * 4.使用ThreadLocal。   √推荐
+     *
+     *
      */
 }
