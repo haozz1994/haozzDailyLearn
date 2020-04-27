0cb9f07f2a21639d844b530fe0dff9552a7b52ab
haozz1994 <haozz0611@163.com>
2020-04-28 00:05:22 +0800

    LockSupport

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202004/dailylearn_20200427/LockSupportTest.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202004/dailylearn_20200427/LockSupportTest.java
@@ -13,7 +13,7 @@ public class LockSupportTest {
      * 在执行LockSupport.park()后，当前线程会被挂起，因为在默认情况下调用线程是不持有LockSupport中的许可证的
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
         // 只会输出begin park
         // 在执行LockSupport.park()后，当前线程会被挂起，因为在默认情况下调用线程是不持有LockSupport中的许可证的
@@ -29,19 +29,38 @@ public class LockSupportTest {
 
         //两个打印都会输出
 
-        System.out.println("bebin park!");
-
-        //使当前线程获得许可证
-        LockSupport.unpark(Thread.currentThread());
-
-        //再次调用park方法
-        LockSupport.park();
-
-        System.out.println("end park!");
+//        System.out.println("bebin park!");
+//
+//        //使当前线程获得许可证
+//        LockSupport.unpark(Thread.currentThread());
+//
+//        //再次调用park方法
+//        LockSupport.park();
+//
+//        System.out.println("end park!");
 
 
 
 
         // ============================================================================================
+        Thread threadOne = new Thread(() ->{
+            System.out.println("child thread begin park!");
+
+            //调用park方法，挂起自己
+            LockSupport.park();
+
+            System.out.println("child thread unpark!");
+        });
+
+        //启动子线程
+        threadOne.start();
+
+        //主线程休眠1s
+        Thread.sleep(1000);
+
+        System.out.println("main thread begin unpark");
+
+        //调用unpark方法讓thread线程持有许可证，然后park方法返回
+        LockSupport.unpark(threadOne);
     }
 }
