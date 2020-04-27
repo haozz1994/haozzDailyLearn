02c0e3818ea43725154baec2f61db8296dc02611
haozz1994 <haozz0611@163.com>
2020-04-27 23:48:14 +0800

    LockSupport

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202004/dailylearn_20200427/LockSupportTest.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202004/dailylearn_20200427/LockSupportTest.java
@@ -8,6 +8,11 @@ import java.util.concurrent.locks.LockSupport;
  */
 public class LockSupportTest {
 
+    /**
+     * 只会输出begin park
+     * 在执行LockSupport.park()后，当前线程会被挂起，因为在默认情况下调用线程是不持有LockSupport中的许可证的
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("begin park!");
