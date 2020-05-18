8221a3f970a1f134da4cbf3142e42557c7445a9f
haozz <haozz0611@163.com>
2020-05-18 14:17:07 +0800

    20200518

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202005/dailylearn_20200518/CountRateLimiterDemo1.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202005/dailylearn_20200518/CountRateLimiterDemo1.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * https://blog.csdn.net/zhuyanlin09/article/details/90762704
+ *
  * @author haozhezhe@yunquna.com
  * @date 14:11 2020/5/18
  */
@@ -12,13 +13,13 @@ public class CountRateLimiterDemo1 {
 
     /**
      * 使用Semaphore信号量来控制并发执行的次数，如果超过域值信号量，则进入阻塞队列中排队等待获取信号量进行执行。如果阻塞队列中排队的请求过多超出系统处理能力，则可以在拒绝请求。
-     *
+     * <p>
      * 相对Atomic优点：如果是瞬时的高并发，可以使请求在阻塞队列中排队，而不是马上拒绝请求，从而达到一个流量削峰的目的。
      */
     private static Semaphore semphore = new Semaphore(5);
 
     public static void exec() {
-        if(semphore.getQueueLength()>100){
+        if (semphore.getQueueLength() > 100) {
             System.out.println("当前等待排队的任务数大于100，请稍候再试...");
         }
         try {
