ad1b9a54e4a553a121194af608ec4c604cddce95
haozz <haozz0611@163.com>
2020-06-13 14:18:59 +0800

    DynamicLoad

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202006/dailylearn_20200613/TestDynamicLoad.java
@@ -0,0 +1,35 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202006.dailylearn_20200613;
+
+public class TestDynamicLoad {
+
+    static {
+        System.out.println("****** load TestDynamicLoad ******");
+    }
+
+    public static void main(String[] args) {
+        new A();
+        System.out.println("****** load test ******");
+        // B不会被加载，除非这里执行new B()
+        B b = null;
+    }
+}
+
+class A {
+    static {
+        System.out.println("****** load A ******");
+    }
+
+    public A() {
+        System.out.println("****** initial A ******");
+    }
+}
+
+class B {
+    static {
+        System.out.println("****** load B ******");
+    }
+
+    public B() {
+        System.out.println("****** initial B ******");
+    }
+}
