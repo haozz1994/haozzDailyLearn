12fa657b0d554b71cf6abd86d090b85219f5e2c5
haozz <haozz0611@163.com>
2020-07-05 13:08:19 +0800

    AllotOnStack

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202007/dailylearn_20200704/User.java
@@ -0,0 +1,13 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202007.dailylearn_20200704;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+
+}
