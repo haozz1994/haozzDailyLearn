e3f892e6a0d570d71e4cb1f1edb259838435e9f5
haozz <haozz0611@163.com>
2020-07-26 22:32:38 +0800

    Arthas

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202007/dailylearn_20200726/Fobernaci.java
@@ -0,0 +1,24 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202007.dailylearn_20200726;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 14:41 2020-07-26
+ */
+public class Fobernaci {
+
+    /**
+     * 1,1,2,3,5,8,13,21,34
+     */
+
+
+    public int calculator(int i) {
+        if (i == 1 || i == 2) {
+            return 1;
+        }
+
+
+
+
+
+    }
+}
