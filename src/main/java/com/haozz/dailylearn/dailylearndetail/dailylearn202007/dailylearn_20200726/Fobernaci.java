77eef33cf454533e47731d61ce5ea4a9fdb1246a
haozz <haozz0611@163.com>
2020-07-30 23:13:51 +0800

    fobernaci

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202007/dailylearn_20200726/Fobernaci.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202007/dailylearn_20200726/Fobernaci.java
@@ -9,16 +9,24 @@ public class Fobernaci {
     /**
      * 1,1,2,3,5,8,13,21,34
      */
-
-
-    public int calculator(int i) {
+    public static int calculator(int i) {
         if (i == 1 || i == 2) {
             return 1;
         }
 
+        return calculator(i-2) + calculator(i-1);
 
+    }
 
-
-
+    public static void main(String[] args) {
+        System.out.println(calculator(1));
+        System.out.println(calculator(2));
+        System.out.println(calculator(3));
+        System.out.println(calculator(4));
+        System.out.println(calculator(5));
+        System.out.println(calculator(6));
+        System.out.println(calculator(7));
+        System.out.println(calculator(8));
+        System.out.println(calculator(9));
     }
 }
