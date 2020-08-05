568bffb9ceb4f997764f9f185eec19e41086f0c5
haozz <haozz0611@163.com>
2020-08-05 21:31:14 +0800

    demo

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202008/dailylearn_20200805/Demo.java
@@ -0,0 +1,18 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202008.dailylearn_20200805;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 21:28 2020-08-05
+ */
+public class Demo {
+
+    /**
+     * 想做一个自己的小demo项目，无论是之前构想的技术博客，或者是一个小的自己喜欢的主题网站，比如海贼王之类
+     *
+     * 主要是想把目前的技术栈用起来
+     *
+     * 如果是技术博客的话，网上应该有成熟的案例和框架之类的
+     *
+     * 如果是网站的话，可能需要一些前端的东西，比如vue或者react之类的
+     */
+}
