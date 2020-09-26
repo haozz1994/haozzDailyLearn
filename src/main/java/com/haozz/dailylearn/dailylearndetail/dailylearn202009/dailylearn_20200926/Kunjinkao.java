6c229e8b3cb72ef0a6ea29353961def0f4d95682
haozz <haozz0611@163.com>
2020-09-26 09:50:17 +0800

    kunjinkao

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202009/dailylearn_20200926/Kunjinkao.java
@@ -0,0 +1,16 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202009.dailylearn_20200926;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 09:49 2020-09-26
+ */
+public class Kunjinkao {
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String str = "��";
+        String strCode = new String(str.getBytes("UTF-8"), "GBK");
+        System.out.println(strCode);
+    }
+}
