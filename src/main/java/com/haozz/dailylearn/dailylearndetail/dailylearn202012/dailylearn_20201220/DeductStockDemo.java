4d9c185ed0cfd66d86afbdc62e61d6d2aa047538
haozz <haozz0611@163.com>
2020-12-20 15:47:35 +0800

    分布式锁

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202012/dailylearn_20201220/DeductStockDemo.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202012/dailylearn_20201220/DeductStockDemo.java
@@ -24,7 +24,9 @@ public class DeductStockDemo {
      *
      * 这段代码是有问题的，会存在超卖现象:
      * 如果两个线程同时执行到了step1，同时拿到当前库存为50，然后同时执行step2进行减1，结果都为49，然后都设置回redis
-     * 这样，实际上进行了两次售卖，但是最终库存只减掉了一份 ，就会存在超卖问题
+     * 这样，实际上进行了两次售卖，最终剩余库存应该是48，但是最终库存只减掉了一份，剩余库存是49，就会存在超卖问题
+     *
+     *
      */
     public void deductStock() {
 
@@ -40,6 +42,38 @@ public class DeductStockDemo {
             System.out.println("扣减成功，剩余库存" + realStock);
         } else {
             System.out.println("扣减失败，库存不足");
+        }
+
+    }
+
+
+    /**
+     * 改进：将step1~step3进行synchronized同步处理，放到一个原子块中，保证同一时间只有一个线程可以执行step1的代码
+     *
+     * 或者使用stringRedisTemplate.opsForValue().increment("stock", -1);方法，它的底层本身也是同步的
+     *
+     * 还有setIfAbsent也是同步的
+     *
+     *
+     */
+    public void deductStock1() {
+
+        synchronized (this){
+            // step1: 查找redis中记录的当前库存
+            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+
+//            stringRedisTemplate.opsForValue().increment("stock", -1);
+
+            if (stock > 0) {
+                // step2: 如果当前库存大于0，则将库存-1
+                int realStock = stock - 1;
+
+                // step3: 将扣减后的库存设置回redis中
+                stringRedisTemplate.opsForValue().set("stock", realStock + "");
+                System.out.println("扣减成功，剩余库存" + realStock);
+            } else {
+                System.out.println("扣减失败，库存不足");
+            }
         }
 
     }
