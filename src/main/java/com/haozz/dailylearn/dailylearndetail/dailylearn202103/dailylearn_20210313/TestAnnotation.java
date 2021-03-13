dfd3605eba5fe5d48f6e9435c9699cb1fabb73ba
haozz <haozz0611@163.com>
2021-03-13 23:40:30 +0800

    annotation

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202103/dailylearn_20210313/TestAnnotation.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202103/dailylearn_20210313/TestAnnotation.java
@@ -32,6 +32,18 @@ public class TestAnnotation {
          * 比如override就只能用在方法上
          *
          * 注解中的字段，就是在使用这个注解是需要定义的参数
+         */
+
+
+        /**
+         *
+         * 元注解：用于注解其他注解。Java中有4个元注解
+         * @Target : 标识当前注解可以用于什么地方
+         * @Retention ：表示需要在什么级别保存该注释信息，用于描述注解的生命周期
+         *          SOURCE (源代码)   <    CLASS (类)     <    RUNTIME(运行时，默认值)
+         *
+         * @Document : 该注解将被包含在javadoc中
+         * @Inherited : 说明子类可以继承父类中的该注解
          *
          *
          *
