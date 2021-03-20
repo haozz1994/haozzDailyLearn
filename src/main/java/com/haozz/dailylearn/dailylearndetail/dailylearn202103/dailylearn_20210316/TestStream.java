93b63797f75cc0ea30df0b7b28ae64682c6cbeb9
haozz <haozz0611@163.com>
2021-03-20 10:46:09 +0800

    optional

--- src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202103/dailylearn_20210316/TestStream.java
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202103/dailylearn_20210316/TestStream.java
@@ -1,8 +1,6 @@
 package com.haozz.dailylearn.dailylearndetail.dailylearn202103.dailylearn_20210316;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -46,9 +44,30 @@ public class TestStream {
         // 统计各个颜色的平均重量
         // collect里面 groupBY， 第一个参数是分组的key也即是颜色，第二个参数是分组之后做的事，计算平均重量
         Map<String, Double> map = list.stream()
-                .collect(Collectors.groupingBy(i -> i.getColor(),Collectors.averagingInt(i -> i.getWeight())));
+                .collect(Collectors.groupingBy(i -> i.getColor(), Collectors.averagingInt(i -> i.getWeight())));
 
         System.out.println(map);
+
+
+        list.stream().sorted(Comparator.comparingInt(t -> t.getColor().length()));
+
+
+        Random random = new Random();
+        random.ints().limit(10).forEach(i -> i = i + 10);
+
+
+        list.parallelStream().sorted();
+
+        // stream 自带的统计效果
+        IntSummaryStatistics summaryStatistics = list.stream().mapToInt(Apple::getWeight).summaryStatistics();
+        System.out.println("列表中最大的数 : " + summaryStatistics.getMax());
+        System.out.println("列表中最小的数 : " + summaryStatistics.getMin());
+        System.out.println("所有数之和 : " + summaryStatistics.getSum());
+        System.out.println("平均数 : " + summaryStatistics.getAverage());
+
+
+
+        Optional.ofNullable(list).ifPresent( i-> System.out.println(i.size()));
 
     }
 }
