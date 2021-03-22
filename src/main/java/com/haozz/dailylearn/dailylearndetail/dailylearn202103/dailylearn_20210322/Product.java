f526e8c3bf6c2531ee828c7d7e25a6428bcd4373
haozz <haozz0611@163.com>
2021-03-22 16:15:48 +0800

    redisson

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202103/dailylearn_20210322/Product.java
@@ -0,0 +1,19 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202103.dailylearn_20210322;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 3:43 PM 3/22/21
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private Long id;
+
+    private String businessKey;
+}
---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202103/dailylearn_20210322/RedissonDemo.java
@@ -0,0 +1,126 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202103.dailylearn_20210322;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.util.function.Tuple2;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 3:29 PM 3/22/21
+ */
+public class RedissonDemo {
+
+    /**
+     * redisson分布式锁demo
+     */
+
+    /**
+     * 引入mvn
+     *        <dependency>
+     *             <groupId>org.redisson</groupId>
+     *             <artifactId>redisson-spring-boot-starter</artifactId>
+     *             <version>3.10.6</version>
+     *         </dependency>
+     */
+
+    /**
+     * @see DeductStockDemo
+     */
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    public final static String KEY_PROFIX = "lock_";
+
+    public void onShelf(List<Product> products) {
+
+        Tuple2<Set<String>, Set<String>> tuple2 = lock(products.stream().map(Product::getBusinessKey).collect(Collectors.toSet()));
+
+        // 成功的部分，执行上架处理，并且在finally中释放锁
+        try {
+            onShelf(tuple2.getT1());
+        } catch (Exception e) {
+
+        } finally {
+            unLock(tuple2.getT1());
+        }
+
+
+        // 失败的部分 递归进行
+        List<Product> collect = products.stream().filter(i -> tuple2.getT2().stream().anyMatch(y -> Objects.equals(i.getBusinessKey(), y))).collect(Collectors.toList());
+        onShelf(collect);
+
+    }
+
+    /**
+     * 商品批量上架
+     * <p>
+     * 在上架的过程中不允许对商品做其他操作，比如调整价格之类
+     */
+    public Tuple2<Set<String>, Set<String>> lock(Set<String> businessKeys) {
+
+
+        Set<String> successSet = new HashSet<>();
+        Set<String> failSet = new HashSet<>();
+
+        // 遍历处理
+        for (String s : businessKeys) {
+
+            // 对每个商品加锁
+            String lockKey = KEY_PROFIX + s;
+
+            // 获取锁对象，此时还没有加锁
+            RLock lock = redissonClient.getLock(lockKey);
+
+            try {
+                /**
+                 * 1. 商品批量获取锁等待时间 单位秒 ，0为不等待，及时返回获取锁的结果
+                 * 2. 商品批量加锁时，单个商品超时解锁时间 单位秒
+                 * 3. 时间单位
+                 */
+
+                boolean isLock = lock.tryLock(0, 60, TimeUnit.SECONDS);
+
+                if (isLock) {
+                    // 获取到锁
+                    successSet.add(s);
+                } else {
+                    //  未获取到锁
+                    failSet.add(s);
+                }
+
+            } catch (Exception e) {
+
+            }
+        }
+
+        return new Tuple2<>(successSet, failSet);
+    }
+
+    public void onShelf(Set<String> businessKeys) {
+        // todo 执行下架操作
+    }
+
+    public void unLock(Set<String> businessKeys) {
+        for (String businessKey : businessKeys) {
+
+            String key = KEY_PROFIX + businessKey;
+            RLock lock = redissonClient.getLock(key);
+            // 释放锁之前，要校验这个锁是不是存在 并且 是上锁状态 并且 是被当前线程持有
+            if (lock == null && lock.isLocked() && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+
+        }
+    }
+
+
+}
