6eb2da358d9fb0a3a0323cd375698bfc39553c72
haozz <haozz0611@163.com>
2021-06-16 00:08:05 +0800

    0615 OOMTest

---
+++ src/main/java/com/haozz/dailylearn/dailylearndetail/dailylearn202106/dailylearn_20210615/OutOfMemeoryDemo.java
@@ -0,0 +1,30 @@
+package com.haozz.dailylearn.dailylearndetail.dailylearn202106.dailylearn_20210615;
+
+import com.haozz.dailylearn.mp.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author haozhezhe@yunquna.com
+ * @date 12:01 AM 6/16/21
+ */
+public class OutOfMemeoryDemo {
+
+
+    public static List<Object> list = new ArrayList<>();
+
+    // JVM设置
+    // -Xms10M -Xmx10M -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:\jvm.dump
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (true) {
+            list.add(new User((long) i++, UUID.randomUUID().toString()));
+            new User((long) j--, UUID.randomUUID().toString());
+        }
+    }
+
+}
--- src/main/java/com/haozz/dailylearn/mp/entity/User.java
+++ src/main/java/com/haozz/dailylearn/mp/entity/User.java
@@ -1,12 +1,16 @@
 package com.haozz.dailylearn.mp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author
  * @date 2020/1/4 15:09
  **/
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private Long id;
@@ -16,4 +20,9 @@ public class User {
     private String name;
 
     private String address;
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
